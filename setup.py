5ac2e8482b3729c2b804480fdf7d9c638b8582b3
andreaschiavinato <andreaschiavinato@hotmail.com>
2019-10-28 19:51:31 +0100

    changed URL on setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pygrabber',
         version='0.1',
         description='Module for grabbing live video images using DirectShow',
-        url='http://ictshore.com/',
+        url='https://github.com/andreaschiavinato/python_grabber',
         author='andreaschiavinato',
         author_email='',
         license='MIT',
