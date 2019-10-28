85d63a75025f944236850a91b97527cf9727c9a9
Scott Pinkham <ByteArts@users.noreply.github.com>
2019-10-28 12:37:06 -0600

    Add files needed to install pygrabber as a module. (#3)

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pygrabber',
+        version='0.1',
+        description='Module for grabbing live video images using DirectShow',
+        url='http://ictshore.com/',
+        author='andreaschiavinato',
+        author_email='',
+        license='MIT',
+        packages=['pygrabber'],
+        zip_safe=False)
