4c80581c42c9cc8dfbc584bfb85fdb7e499cc771
Joao Paulo Paiva <joaopaulo.ps93@gmail.com>
2020-09-23 08:36:05 -0400

    Update setup.py to accept newer versions of required packages. (#10)

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(name='pygrabber',
         license='MIT',
         packages=['pygrabber'],
         install_requires=[
-            "comtypes==1.1.7",
-            "numpy==1.17.3",
-            "opencv-python==4.1.1.26"
+            "comtypes>=1.1.7",
+            "numpy>=1.17.3",
+            "opencv-python>=4.1.1.26"
         ],
         zip_safe=False)
