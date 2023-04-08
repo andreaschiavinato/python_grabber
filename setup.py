bb1ef0d39da9d0f39593ba7e54c2d72df0bbbb65
Avasam <samuel.06@hotmail.com>
2023-04-08 00:46:33 -0400

    Add README as PyPI long_description
    Attempt to fix #19

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
+from pathlib import Path
 from setuptools import setup
 
 setup(name='pygrabber',
       version='0.1',
       description='Module for grabbing live video images using DirectShow',
-      long_description='Module for grabbing live video images using DirectShow',
+      long_description=(Path(__file__).parent / "README.md").read_text(),
+      long_description_content_type='text/markdown'
       url='https://github.com/andreaschiavinato/python_grabber',
       author='andreaschiavinato',
       author_email='',
